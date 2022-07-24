a110338cee4c6f3ccf083a0cdca035bb04e4ac78
amfarari <anish233228@gmail.com>
2022-07-25 02:20:59 +0530

    Whatsapp API

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ const express = require("express");
 const app = express();
 const port = prosess.env.PORT || 3000;
 
-const qrcode = require('qrcode-terminal');
+// const qrcode = require('qrcode-terminal');
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
+// const { Client, LocalAuth } = require('whatsapp-web.js');
 
-const client = new Client({
-    authStrategy: new LocalAuth()
-});
+// const client = new Client({
+//     authStrategy: new LocalAuth()
+// });
 
 
-client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
-});
+// client.on('qr', qr => {
+//     qrcode.generate(qr, { small: true });
+// });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-let num = "7619876249";
+// client.on('ready', () => {
+//     console.log('Client is ready!');
+// // });
+// let num = "7619876249";
 
-client.initialize();
+// client.initialize();
 
 app.get("/", (req, res) => {
-    client.sendMessage("91" + num + "@c.us", "Hello This is Vivek Kumar Yadav");
-    res.send("Masses Is Send to " + num);
+    // client.sendMessage("91" + num + "@c.us", "Hello This is Vivek Kumar Yadav");
+    res.send("Masses Is Send to ");
 });
 
 app.listen(port, () => {
