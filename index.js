d16eec2fda2a6676e35825f6a1bf1f08431f1a15
amfarari <anish233228@gmail.com>
2022-07-25 02:00:59 +0530

    Whatsapp API

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
-const port = prosess.env.PORT || 3000;
+const port = 3000;
 
+const qrcode = require('qrcode-terminal');
 
-app.get("/", (req, res) => {
-    res.send("Hello W");
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
 });
 
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+let num = "7619876249";
+
+client.initialize();
+
+app.get("/", (req, res) => {
+    client.sendMessage("91" + num + "@c.us", "Hello This is Vivek Kumar Yadav");
+    res.send("Masses Is Send to " + num);
+});
 
 app.listen(port, () => {
     console.log("Live");
